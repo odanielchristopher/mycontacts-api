ae44a8fc10a3c4d606a55971a1483f1c07e8d4ca
Daniel Rodrigues <o.dani.br@gmail.com>
2023-12-13 15:22:06 -0300

    criando update

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -19,7 +19,24 @@ class CategoryController {
     response.json(category);
   }
 
-  update() {}
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(400).json({ error: 'Category not exists' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    response.json(category);
+  }
 
   delete() {}
 }
