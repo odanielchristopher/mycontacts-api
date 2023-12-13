2c5d182602fdde0955ab4aa7ae4a7ed235b03d2f
Daniel Rodrigues <o.dani.br@gmail.com>
2023-12-13 14:27:02 -0300

    refatorando delete

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -85,12 +85,6 @@ class ContactController {
     // Deletar um registro
     const { id } = request.params;
 
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      return response.status(404).json({ error: 'Contact not found' });
-    }
-
     await ContactsRepository.delete(id);
     // 204: No Content
     response.sendStatus(204);
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Daniel',
-    email: 'dani@mail.com',
-    phone: '1212121212',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Jonas',
-    email: 'jonsh@mail.com',
-    phone: '123412123',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -42,11 +23,12 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query(`
+      DELETE FROM contacts
+      WHERE id = $1
+    `, [id]);
+    return deleteOp;
   }
 
   async create({
