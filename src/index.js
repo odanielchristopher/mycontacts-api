d54b0d4a19b30b279813210718310167183a4132
Daniel Rodrigues <o.dani.br@gmail.com>
2024-03-18 12:50:46 -0300

    criando o CORS middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
 const routes = require('./routes');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 // Error Handler => Middleware Express
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(3000, () => console.log('🤪 Server stated at http://localhost:3000'));
+app.listen(3001, () => console.log('🤪 Server stated at http://localhost:3001'));
